079f7556888ab74e5e1520c89b9c98923ef3da94
Erfangh_80 <Erfangh4871gholami@gmail.com>
2022-01-18 17:14:40 +0330

    add controller history to project

---
+++ controllers/momentHistoryController.js
@@ -0,0 +1,34 @@
+import momentServices from "../services/momentHistoryService.js"
+
+class momentHistoryController {
+
+  async createMoment(req,res){
+    const {symbolId, userId, momentDate, numberTransaction, valueTransaction, volumeTransaction, priceMin, priceMax} = req.body;
+
+    try {
+      const newMomentHistory = await momentServices.insertMomentHistory(symbolId, userId, momentDate, numberTransaction, valueTransaction, volumeTransaction, priceMin, priceMax);
+      
+      res.status(201).json(newMomentHistory);
+
+    } catch (error) {
+      res.status(401).json({ message: error.message });
+    }
+
+  }
+
+  async getAllMomentHistory(req,res){
+
+    try {
+      const allMomentHistory = await momentServices.getAllHistoryMoment();
+      if (allMomentHistory.length == 0) {
+        res.status(200).json({ message: 'هیچ تاریخچه ای وجود ندارد' })
+      }
+      res.status(200).json(allMomentHistory);
+
+    } catch (error) {
+      res.status(404).json({ message: error.message })
+    }
+  }
+
+  
+}
