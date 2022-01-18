a960d86d310cd80743850860e20afeb05b1eb90a
Erfangh_80 <Erfangh4871gholami@gmail.com>
2022-01-18 14:38:58 +0330

    add model db momentHistory to project

--- models/momentHistory.js
+++ models/momentHistory.js
@@ -1,15 +1,15 @@
-import mongoose, { Types } from 'mongoose';
+import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
 const momentHistory = new Schema({
   symbolId : {
-    type : Types.ObjectId,
+    type : Number,
     ref : 'Symbol',
     required : true
   },
   userId : {
-    type : Types.ObjectId,
+    type : Number,
     ref : 'User',
     required : true
   },
